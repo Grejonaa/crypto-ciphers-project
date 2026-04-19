453f12d022146e34a16b2e8c93ec039ea7181149
elmazemuratii <elmaze.murati@student.uni-pr.edu>
2026-04-19 14:33:50 +0200

    Autokey cipher encryption

--- src/autokey_cipher.py
+++ src/autokey_cipher.py
@@ -2,37 +2,21 @@ def autokey_encrypt(plaintext, key):
     plaintext = plaintext.upper()
     key = key.upper()
 
-    full_key = key + plaintext
+    if not key:
+        raise ValueError("Key nuk mund të jetë bosh!")
+
     cipher = ""
+    full_key = key
 
-    for i in range(len(plaintext)):
-        p = ord(plaintext[i]) - ord('A')
-        k = ord(full_key[i]) - ord('A')
+    for ch in plaintext:
+        if ch.isalpha():
+            p = ord(ch) - ord('A')
+            k = ord(full_key[0]) - ord('A')
+            full_key = full_key[1:] + ch
 
-        c = (p + k) % 26
-        cipher += chr(c + ord('A'))
+            c = (p + k) % 26
+            cipher += chr(c + ord('A'))
+        else:
+            cipher += ch
 
     return cipher
-
-
-def autokey_decrypt(ciphertext, key):
-    ciphertext = ciphertext.upper()
-    key = key.upper()
-
-    plaintext = ""
-
-    for i in range(len(ciphertext)):
-        k = ord(key[i]) - ord('A')
-        c = ord(ciphertext[i]) - ord('A')
-
-        p = (c - k) % 26
-        letter = chr(p + ord('A'))
-
-        plaintext += letter
-        key += letter 
-
-    return plaintext
-
-
-
-
